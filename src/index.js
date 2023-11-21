f39a1d0e35f8a2136835592c4f6f6bdb8bf2d540
ElianGalindo <elianalejandro202@gmail.com>
2023-11-21 16:19:56 -0600

    Hicimos login

---
+++ src/index.js
@@ -0,0 +1,110 @@
+import express from 'express'
+import bcrypt from 'bcrypt'
+import cors from 'cors'
+import { initializeApp } from 'firebase/app'
+import { collection, deleteDoc, doc, getDoc, getDocs, getFirestore, setDoc, updateDoc } from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB9KxfukWbezFagY0OKypgPmIXxJgGEPUU",
+    authDomain: "clinica-eb-978a5.firebaseapp.com",
+    projectId: "clinica-eb-978a5",
+    storageBucket: "clinica-eb-978a5.appspot.com",
+    messagingSenderId: "139481584728",
+    appId: "1:139481584728:web:8389d7e471fe580b4ae62d"
+}
+
+const firebase = initializeApp(firebaseConfig)
+const db = getFirestore(firebase)
+
+//Inicializar el servidor
+const app = express()
+
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+
+}
+
+app.use(cors(corsOptions))
+app.use(express.json())
+
+//Ruta para login
+app.post('/login', (req, res) => {
+    const { email, password } = req.body
+    console.log('Datos recibidos en la solicitud:', req.body);
+
+    if(!email || !password) {
+        res.json({
+            'alert':'faltan datos'
+        })
+    }
+
+    const usuarios = collection(db, 'usuarios')
+    getDoc(doc(usuarios, email))
+    .then((usuario) => {
+        if(!usuario.exists()) {
+            res.json({ 'alert':'Correo no registrado' })
+        }else {
+            bcrypt.compare(password, usuario.data().password, (error, result) => {
+                if(result){
+                    //Para regresar datos
+                    let data = usuario.data()
+                    res.json({ 
+                        'alert':'success',
+                        email: data.email,
+                    })
+                } else{
+                    res.json({ 'alert':'Contrasena Incorrecta'})
+                }
+            })
+        }
+    })
+})
+
+//Registra usuario
+app.post('/new-user', (req, res) => {
+    let {email, password} = req.body
+    
+    if(!email.length){
+        res.json({
+            'alerta': 'Falta el usuario'
+        })
+    }else if(!password.length){
+        res.json({
+            'alerta': 'Falta el password'
+        })
+    }
+
+    const usuarios = collection(db, 'usuarios')
+
+    getDoc(doc(usuarios, email)).then(user => {
+        if (user.exists()){
+            res.json({
+                'alert': 'El usuario ya existe'
+            })
+        }else{
+            //encriptar contrasenia
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(password, salt, (err, hash) => {
+                    const data = {
+                        email,
+                        password: hash
+                    }
+                    setDoc(doc(usuarios, email), data).then(data => {
+                        res.json({
+                            'alert': 'success',
+                            data
+                        })
+                    })
+                })
+            })
+        }
+    }).catch(error => {
+        res.json({
+            'alert': 'Error de conexion'
+        })
+    })
+})
+app.listen(5020, () => {
+    console.log('Servidor trabajando: 5020')
+})
